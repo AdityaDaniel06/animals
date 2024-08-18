94afc5b8d94cb5c84fa24f755143bb06f3d4d7ce
AdityaDaniel06 <danieladitya327@gmail.com>
2024-08-18 16:43:16 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimals() {
+  const animals = ["dog", "cat", "horse", "cow", "gator"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
+  // Event handler fn
+  // const handleClick =()=> {
+  //     console.log('Button was clicked');
+  // };
+  //   const [count, setCount] = useState(0);
+
+  //   const handleClick = () => {
+  //     setCount(count + 1);
+  //   };
+  const [animals, setAnimals] = useState([]);
+  const handleClickAnimal = () => {
+    setAnimals([...animals, getRandomAnimals()]);
+  };
+
+  const renderAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <button onClick={()=> console.log('Button was clicked')}>Add Animals</button> */}
+      {/* <div>
+        <button onClick={handleClick}>Add Animals</button>
+        Number of animals = {count}
+      </div> */}
+      <div className="app">
+        <button onClick={handleClickAnimal}> Add Animals</button>
+        <div className="animal-list"> {renderAnimals}</div>
+      </div>
     </div>
   );
 }
